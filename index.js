97c4375234d73e738e15cbfbb9529b70ef2b438c
Kayla Phelps <kmphelps822@gmail.com>
2021-07-22 15:13:31 -0700

    commit tree efd21581e91c98e9bb476c186b734dfe410c35ef

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "1969", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
+
+function findWin(item) {
+    return item.result === "W";
+}
+
+function superbowlWin(array) {
+    const record = array.find(findWin);
+    return record ? record.year : undefined;
+}
+
+
+
+
+
 
